1692e6f08e739998cb86d79d384af7b697c218e0
LeoChen-OSC <st25246@ormiston.school.nz>
2026-08-19 13:45:55 +1200

    added a donut slideshow to the main page

--- static/script.js
+++ static/script.js
@@ -1,7 +1,23 @@
+new Swiper("#swiper-1", {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 function updatePaymentForm() {
+    //sets const variables for the delivery method and address input field.
     const deliveryMethod = document.getElementById('delivery_method').value;
     const addressInput = document.querySelector('input[name="address"]');
-
+    // fetches the address form and determines if delivery methods is delivery or pickup. 
     if (deliveryMethod === "delivery") {
         addressInput.required = true;
         addressInput.placeholder = "Enter your address (required)";
